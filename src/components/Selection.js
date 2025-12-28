6bd40c2e4dbacb7ba8dc3db2be693583440b1337
VeeraMahendra7 <veeramahindra787@gmail.com>
2025-12-28 10:28:35 +0530

    Done

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -6,8 +6,8 @@ const Selection = ({applyColor}) => {
     const [style, setStyle] = useState({background:""})
     
   return (
-    <div className="fix-box" style={style} onClick={() => applyColor(setStyle)}>
-      
+    <div className='fix-box' style={style} onClick={() => applyColor(setStyle)}>
+        <h1 className='heading'>Selection</h1>
     </div>
   )
 }
